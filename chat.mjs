0b4425eb70f7c95bbec7b0a26e7a105de088ac3e
PaolaLlh18 <paola.llamash18@gmail.com>
2025-06-14 22:55:01 -0600

    chatichy

--- chat.mjs
+++ chat.mjs
@@ -1,16 +1,23 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import readline from "readline";
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
 
-async function main() {
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("¿Qué quieres preguntarle a Ichy?\n> ", async (pregunta) => {
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: "Hablame brevemente de Arca Continental",
+    contents: pregunta,
   });
-  console.log(response.text);
-}
 
-await main();
+  console.log("\nRespuesta de Ichy:\n", response.text);
+  rl.close();
+});
+
