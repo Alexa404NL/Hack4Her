138e63e5959d9a751c263d5de527f457ca09d5b6
PaolaLlh18 <paola.llamash18@gmail.com>
2025-06-14 22:35:26 -0600

    chatbien

--- chat.mjs
+++ chat.mjs
@@ -1,13 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI({ apiKey: "API_KEY" });
+const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
 
 async function main() {
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: "You are a worm that works very hard like the bees",
+    contents: "Hablame brevemente de Arca Continental",
   });
   console.log(response.text);
 }
 
-main();
+await main();
