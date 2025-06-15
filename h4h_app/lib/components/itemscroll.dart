8d5b58ec20505fd20011ce01eb6bb907e8a27c53
Emily Joanne Castillo Martinez <a00839559@tec.mx>
2025-06-15 01:35:35 -0600

    Scrollbar y PageView Modificaciones

--- h4h_app/lib/components/itemscroll.dart
+++ h4h_app/lib/components/itemscroll.dart
@@ -7,42 +7,50 @@ Widget itemListView() {
       padding: EdgeInsets.fromLTRB(6,0,6,0),
       scrollDirection: Axis.horizontal,
       children: [
-        itemTile("Item 1"),
-        itemTile("Item 2"),
-        itemTile("Item 3"),
-        itemTile("Item 4"),
-        itemTile("Item 5")
+        itemTile("Pedido Fácil"),
+        itemTile("Refrescos"),
+        itemTile("Agua"),
+        itemTile("Bebidas de fruta"),
+        itemTile("Agua mineral")
       ],
     ),
   );
 }
 
 Widget itemTile(String titlet) {
-  return Center(
-    child: Column(
-      children: [
-        Container(
-          width: 118,
-          height: 118,
-          margin: EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(15.0),
-            boxShadow: [BoxShadow(
-                color: Colors.black.withValues(alpha: 0.1), // light shadow
-                blurRadius: 5,
-                offset: Offset(0, 3),
-            )],
-          ),
-          child: Center(child: Text("[image]")),
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: SizedBox(
+      width: 95,
+      child: Center(
+        child: Column(
+          spacing: 11,
+          children: [
+            Container(
+              width: 95,
+              height: 95,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [BoxShadow(
+                    color: const Color.fromARGB(255, 58, 58, 58).withValues(alpha: 0.1), // light shadow
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                )],
+              ),
+              child: Center(child: Text("[image]", softWrap: true)),
+            ),
+            Text(titlet,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+                color: Color(0xff4c4b4a),
+                ),
+            ),
+          ],
         ),
-        Text(titlet,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 18
-          ),
-        ),
-      ],
+      ),
     ),
   );
 }
